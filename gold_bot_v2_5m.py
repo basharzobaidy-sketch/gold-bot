e32ce54606df6b319bbd482690f689bb56175154
basharzobaidy-sketch <basharzobaidy@gmail.com>
2026-04-19 21:14:27 +0300

    Update gold_bot_v2_5m.py

--- gold_bot_v2_5m.py
+++ gold_bot_v2_5m.py
@@ -47,3 +47,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run_server():
+    app.run(host='0.0.0.0', port=10000)
+
+threading.Thread(target=run_server).start()
